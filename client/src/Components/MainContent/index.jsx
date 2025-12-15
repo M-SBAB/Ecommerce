987c926d08b0622e2203cf68f1d762ec058d353e
Muhammad Sohail <sohailbinallahbakhsh@gmail.com>
2025-12-15 20:33:55 +0500

    responsiveness: step3, Fix MainContent layout and spacing

--- client/src/Components/MainContent/index.jsx
+++ client/src/Components/MainContent/index.jsx
@@ -7,7 +7,9 @@ const MainContent = ({ toggleSidebar }) => {
     <div className='flex-1 flex flex-col min-h-screen overflow-hidden'>
       <Navbar onMenuClick={toggleSidebar} />
       <div className='flex-1 overflow-auto bg-gray-50'>
-        <Outlet />
+        <div className='max-w-7xl mx-auto p-4 md:p-6 lg:p-8'>
+          <Outlet />
+        </div>
       </div>
     </div>
   );
