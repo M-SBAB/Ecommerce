6d9f9adca3a3b717ed40dcab90b1df54bbfc4769
Muhammad Sohail <sohailbinallahbakhsh@gmail.com>
2025-12-15 22:36:31 +0500

    apply paddings from sourdings to main outlet component

--- client/src/Components/MainContent/index.jsx
+++ client/src/Components/MainContent/index.jsx
@@ -7,7 +7,7 @@ const MainContent = ({ toggleSidebar }) => {
     <div className='flex-1 flex flex-col min-h-screen overflow-hidden'>
       <Navbar onMenuClick={toggleSidebar} />
       <div className='flex-1 overflow-auto bg-gray-50'>
-        <div className='max-w-7xl mx-auto p-4 md:p-6 lg:p-8'>
+        <div className='px-4 sm:px-6 lg:px-8 py-6 sm:py-8 lg:py-10'>
           <Outlet />
         </div>
       </div>
